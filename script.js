56b87c89a5cb046031d6484202368c82ef653c82
JOSE DE JESUS SANCHEZ AGUILAR <jsanc239@itam.mx>
2019-03-28 09:26:15 -0600

    ejercicio idiomas

--- script.js
+++ script.js
@@ -42,6 +42,57 @@ function generaTarjeta() {
     
     var element = document.getElementById("tarjetas");
     element.appendChild(div);
-    
+}
 
+function getLanguages() {
+    const Http = new XMLHttpRequest();
+    const url = "https://apiidiomas.firebaseapp.com/idiomas.json";
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange=(e)=>{
+        inserta(Http.responseText);
+        alert(Http.responseText);
+    }
+}
+
+function inserta(string){
+    var div = document.createElement("div");
+    div.textContent = string;
+    var element = document.getElementById("body");
+    element.appendChild(div);
+}
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+
+function prueba(){
+    var xhr = createCORSRequest("GET", "https://apiidiomas.firebaseapp.com/idiomas.json");
+    alert(xhr.status);
+    
+    if (!xhr) {
+        throw new Error('CORS not supported');
+    }
 }
